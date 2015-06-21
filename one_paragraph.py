5f5d7546a5dafa6d8c8d080f8511d5e6c1dad008
Yoonseop Kang <e0engoon@gmail.com>
2015-06-21 23:33:30 +0900

    add basic design, login, logout

--- one_paragraph.py
+++ one_paragraph.py
@@ -1,15 +1,44 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from flask import Flask
+from flask import (Flask, render_template, redirect, url_for, request,
+                   session)
+from functools import wraps
 
 
 app = Flask(__name__)
+app.secret_key = "secret_key"
+
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if "email" not in session:
+            return redirect(url_for("login", next=request.url))
+        return f(*args, **kwargs)
+    return decorated_function
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html", title="Login: 1Paragraph")
+    else:
+        session["email"] = request.form["email"]
+        return redirect(request.args.get("next"))
+
+
+@app.route("/logout")
+@login_required
+def logout():
+    del session["email"]
+    return redirect(url_for("index"))
 
 
 @app.route("/")
+@login_required
 def index():
-    return "Hello, world!"
+    return render_template("main.html", title="1Paragraph")
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
